f2581dd8e54c16acfb0f256115bfb5b181f41d14
lucyfoxxy <lucy@foxx.pet>
2025-10-22 17:48:16 +0200

    new bg

--- app/dev/src/assets/ui.ts
+++ app/dev/src/assets/ui.ts
@@ -1,10 +1,15 @@
 import type { ImageMetadata } from 'astro';
-import bg from '@Assets/ui/bg.webp';
+import bg from '@Assets/ui/bg9.webp';
 import logo_header from '@Assets/ui/logo-header.webp';
 import logo_hero from '@Assets/ui/logo-hero.webp';
 import favicon from '@Assets/ui/favicon.webp';
 import og from '@Assets/ui/default-og.webp';
-
+import bg__stars from '@Assets/ui/stars.webp';
+import bg__twinkle from '@Assets/ui/twinkle.webp';
+import bg__nebula from '@Assets/ui/nebula.webp';
+import container_bg from '@Assets/ui/container-bg.webp';
 export const ASSETS: Record<string, ImageMetadata> = {
-  favicon, bg, logo_header, logo_hero, og,
+  favicon, og,
+  bg, container_bg, bg__stars, bg__nebula, bg__twinkle, 
+  logo_header, logo_hero, 
 };
