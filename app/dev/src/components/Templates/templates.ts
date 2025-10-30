0ad065eed5d8326afed6ab964f9ff67b9190c8ff
lucyfoxxy <lucy@foxx.pet>
2025-10-30 01:54:32 +0100

    ,

---
+++ app/dev/src/components/Templates/templates.ts
@@ -0,0 +1,10 @@
+import Footer from './partials/Footer.astro';
+import Meta from '@Templates/partials/Meta.astro';
+import Header from '@Templates/partials/Header.astro';
+import Card from '@Templates/Card.astro';
+import TitleWithIcon from './partials/TitleWithIcon.astro';
+import TileView from './partials/TileView.astro'
+import HeaderActions from './partials/HeaderActions.astro';
+export const TEMPLATES = {
+    Header,Footer,Meta,Card,TitleWithIcon,TileView,HeaderActions
+}
