eb88852e27804e17d9b0ee6c14fb8c751e11ef69
lucyfoxxy <lucy@foxx.pet>
2025-11-16 20:32:32 +0100

    added images, cleaned stuff up

--- app/dev/src/assets/ui.ts
+++ app/dev/src/assets/ui.ts
@@ -1,14 +1,14 @@
 import type { ImageMetadata } from 'astro';
-import logo_header from '@Assets/ui/logo-header.webp';
+import mascot_v2 from '@Assets/ui/fox_in_space.webp';
 import logo_hero from '@Assets/ui/logo-hero.webp';
 import favicon from '@Assets/ui/favicon.webp';
 import og from '@Assets/ui/default-og.webp';
 import overlay__stars from '@Assets/ui/stars.webp';
 import overlay__nebula from '@Assets/ui/nebula.webp';
-import bg__panel from '@Assets/ui/container-bg.webp';
+import mascot from '@Assets/ui/hero.webp';
 export const ASSETS: Record<string, ImageMetadata> = {
   favicon, og,
-  bg__panel, 
+  mascot, 
   overlay__stars, overlay__nebula, 
-  logo_header, logo_hero,
+  mascot_v2, logo_hero,
 };
